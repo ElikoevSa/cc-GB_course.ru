9cbf295e6a2ea80b812024956f0a1737ce69d5ee
Aleksandr Zhuk <tradesolokarel@mail.ru>
2023-09-16 23:16:59 +0300

    refactoring Example 002, adeded Random().Next()

--- Examples/Еxample003_Sum/Program.cs
+++ Examples/Еxample003_Sum/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int numberA = new Random().Next(0,11);
+Console.WriteLine("NubberA = " + numberA);
+int numberВ = new Random().Next(0,11);
+Console.WriteLine("NumberB = " + numberВ);
+int result = numberA + numberВ;
+Console.WriteLine(result);
