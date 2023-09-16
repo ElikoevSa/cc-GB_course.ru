64d83c5fc91339547c11b5786705fc30b6763724
Aleksandr Zhuk <tradesolokarel@mail.ru>
2023-09-16 23:27:56 +0300

    adeded  code in Еxample005_ConditionIfElse

--- Examples/Еxample005_ConditionIfElse/Program.cs
+++ Examples/Еxample005_ConditionIfElse/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter You name: ");
+string username = Console.Readline();
+if(username == "Маша"){
+    Console.WriteLine("Это же МАША!!!");
+
+}
+else {
+    Console.WriteLine("HI You name is: " + username);
+}
