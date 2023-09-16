3635533c2b48c414fe3bf76b04612ac80fe62096
Aleksandr Zhuk <tradesolokarel@mail.ru>
2023-09-16 23:07:04 +0300

    adeded new files and .gitignore

---
+++ Examples/Example002_НelloUser/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter you name");
+string username = Console.ReadLine();
+Console.WriteLine("Привет ");
+Console.WriteLine(username);
+
+
