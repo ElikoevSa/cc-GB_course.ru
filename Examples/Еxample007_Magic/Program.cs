688ba66c7fb54150ac3914f60c0aae6c5fb9d209
Aleksandr Zhuk <tradesolokarel@mail.ru>
2023-09-17 00:26:13 +0300

    Edit logik in Еxample007_Magic

--- Examples/Еxample007_Magic/Program.cs
+++ Examples/Еxample007_Magic/Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Clear();
+
+int xa = 1; 
+int ya = 1;
+int xb = 1; 
+int yb = 30;
+int xc = 40; 
+int yc = 30;
+int count = 0;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa;
+int y = ya;
+
+while( count < 1000)
+{
+    int rint = new Random().Next(0,3);
+    if (rint == 0){
+        x = (x +xa)/2;
+        y = (y+ya)/2;
+    }
+    if (rint==1){
+        x = (x+xb) /2;
+        y = (y+yb)/2;
+        }
+    if(rint == 2){
+        x = (x+xc)/2;
+        y= (y+yc)/2;
+    }
+    Console.SetCursorPosition(x,y);
+    Console.WriteLine("+");
+    count++;
+}
