44d065a7cf7aedb05c56c9e88af6e243a248baca
Aleksandr Zhuk <tradesolokarel@mail.ru>
2023-09-22 10:56:07 +0300

    adeded Logic in Example 010

--- Examples/Example010_Method_Array/Program.cs
+++ Examples/Example010_Method_Array/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [] array = {1,4,23,34,5,6,34,3,2,1,76,84,44553,32,3};
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+
+    }
+    index++;
+}
+
+
+
