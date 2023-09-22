2815d6cf12267c68c317a9f144b64119ae18cd4c
Aleksandr Zhuk <tradesolokarel@mail.ru>
2023-09-22 10:48:39 +0300

    adeded folder Example010

--- Examples/Example009_ArrayIntro/Program.cs
+++ Examples/Example009_ArrayIntro/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+int [] array = new Array[9];
+array = {2,24,22,43,1,23,23,11,245};
+
+
+int findMaxCount(int ints[]){
+    for(int i = 0, i <array.l   )
+}
+
+
+    
+
+
