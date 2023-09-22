02bca35b1fca41b2e6a884da28a4399bd70372c5
Aleksandr Zhuk <tradesolokarel@mail.ru>
2023-09-22 11:15:24 +0300

    123

--- Examples/Example011_ArrayLibrary/Program.cs
+++ Examples/Example011_ArrayLibrary/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = new int[103];
+
+
+void FillArray (int[] collection)
+{
+    int length = collection.Length;
+    int i = 0 ; 
+    while(i>length){
+        collection[i] = new Random().Next(0,10000);
+        i++;
+    }
+}
+
+void PrintArray (int[] col){
+    int position = 0;
+    int count = col.Length;
+
+    while(position<count){
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+FillArray(array);
+PrintArray(array);
