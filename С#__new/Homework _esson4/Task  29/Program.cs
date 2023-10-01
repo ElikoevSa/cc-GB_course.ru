cb52dcdc19149ebfa77b8f22450cbfa9b7268a6f
Aleksandr Zhuk <tradesolokarel@mail.ru>
2023-10-01 12:07:14 +0300

    Задача 29 complited

--- С#__new/Homework _esson4/Task  29/Program.cs
+++ С#__new/Homework _esson4/Task  29/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int arraySize = 8;
+
+void PrintArray(int[] array){
+    String result = String.Join(",",array);
+    Console.WriteLine(result);
+}
+
+int[] getArray(int arraySize){
+       int[] arr = new int[arraySize];
+        for (int i = 0; i <arr.Length; i++)
+        {
+            arr[i] = new Random().Next(1,100);
+        }
+        return arr;
+}
+
+PrintArray(getArray(arraySize));
+
+
