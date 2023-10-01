bedc1aa153ab6d81be1a90b3342afd65c3d53aef
Aleksandr Zhuk <tradesolokarel@mail.ru>
2023-10-01 11:18:12 +0300

    task 25 complited

--- С#__new/Homework _esson4/Task 25/Program.cs
+++ С#__new/Homework _esson4/Task 25/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+void powNumbers(int A, int B)
+{
+    int result = A;
+    for (int i = 1; i < B; i++)
+    {
+         result = result*A;
+    }
+Console.WriteLine(result);
+}
+
+double powNumbersMath(double A, double B){
+    return Math.Pow(A,B);
+}
+
+powNumbers(2,4);
+System.Console.WriteLine(powNumbersMath(2.2,2));
