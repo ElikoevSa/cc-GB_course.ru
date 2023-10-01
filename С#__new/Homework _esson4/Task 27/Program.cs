1f91dddba2fd6388c02283adc465227163364c45
Aleksandr Zhuk <tradesolokarel@mail.ru>
2023-10-01 11:37:53 +0300

    task 27 complite

--- С#__new/Homework _esson4/Task 27/Program.cs
+++ С#__new/Homework _esson4/Task 27/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumorDigits(int number){
+    int result = 0;
+    while (number != 0)
+    {
+         result += number % 10;
+         number /= 10;
+    }
+    return result;
+}
+
+
+
+System.Console.WriteLine(SumorDigits(1234));
