326bada31b3477e2b5065444a25dc650df8ec589
Aleksandr Zhuk <tradesolokarel@mail.ru>
2023-10-09 15:12:15 +0300

    task 41 complite

--- С#__new/HomeWorkLesson_6/Task 41/Program.cs
+++ С#__new/HomeWorkLesson_6/Task 41/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+using System;
+
+void EnteredNumber (){
+        Console.Write("Введите числа через запятую: ");
+        string inputNumber = Console.ReadLine();
+
+        string[] numbers = inputNumber.Split(',');
+
+        int count = 0;
+        foreach (string number in numbers)
+        {
+            int num = Convert.ToInt32(number.Trim());
+            if (num > 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Количество чисел больше 0: " + count);
+}
+
+//     System.Console.WriteLine("Введите значение от " + Int64.MinValue + " до " + Int64.MaxValue + ":");
+
+
+//     ArrayList userNumbers = new ArrayList();
+//     string userNumber = Console.ReadLine();
+
+  
+
+
+//     // try
+//     // {
+//     // long userNumber = Convert.ToInt64(Console.ReadLine());
+        
+//     // }
+//     // catch (System.Exception)
+//     // {
+//     //     System.Console.WriteLine("Введён некорректный формат данных, " + 
+//     //     "пожалуйста введите значение от " + Int64.MinValue + 
+//     //     " до " + Int64.MaxValue + ":");
+
+        
+//     // }
+//     // finally {
+//     //     long userNumber = Convert.ToInt64(Console.ReadLine());
+//     // }
+
+        
+
+//     return userNumbers;
+// } 
+
+EnteredNumber();
