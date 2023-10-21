c8ed9a7163b074e3dfc80d2345acabb696178ce5
Aleksandr Zhuk <tradesolokarel@mail.ru>
2023-10-21 11:10:21 +0300

    много разных изменений

--- С#__new/Program.cs
+++ С#__new/Program.cs
@@ -376,3 +376,397 @@
 // {
 //   System.Console.Write(array[i]+ " ");
 // }
+// Задача 46: Задайте двумерный массив размером
+//  m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19вщ
+// 5 -2 33 -2
+// // 77 3 8 1
+
+// Console.WriteLine("Введите размероность массива");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите размероность массива");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// int [,] array = new int[rows,columns];   // Строчек + столбцы
+
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         array[i,j] = new Random().Next(0,10);
+//         Console.Write(array[i,j]+ " ");
+//     }
+//     Console.WriteLine();
+// }
+// Задача 48: Задайте двумерный массив размера
+//  m на n, каждый элемент в массиве находится 
+//  по формуле: Aₘₙ = m+n. Выведите полученный
+//   массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3 
+// 1 2 3 4
+// 2 3 4 5   2
+
+// Console.WriteLine("Введите размерность массива:");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите размерность массива:");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// int[,] arr = new int[rows, columns];
+
+// for (int i = 0; i < rows; i++)
+// {
+//     for (int j = 0; j < columns; j++)
+//     {
+//         arr[i, j] = i + j;
+//         Console.Write(arr[i, j] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// Задача 49: Задайте двумерный массив. Найдите 
+// элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+// Например, изначально массив
+//  выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// 0 1 2 3 
+
+// 1 4 7 2    0
+// 5 9 2 3    1
+// 8 4 2 4    2
+
+// Console.WriteLine("Введите размероность массива");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите размероность массива");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// int [,] array = new int[rows,columns];   // Строчек + столбцы
+
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         array[i,j] = new Random().Next(0,10);
+//         Console.Write(array[i,j]+ " ");
+//     }
+//     Console.WriteLine();
+// }
+//  Console.WriteLine();
+
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         if(i%2 ==0 && j%2==0)
+//         {
+//             array[i,j] = array[i,j]* array[i,j];
+//         }
+//         Console.Write(array[i,j]+ " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// Задача 51: Задайте двумерный массив. 
+// Найдите сумму элементов, находящихся 
+// на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+
+
+// Console.WriteLine("Введите размероность массива");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите размероность массива");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// int [,] array = new int[rows,columns];   // Строчек + столбцы
+
+// int sum=0;
+
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         array[i,j] = new Random().Next(0,10);
+//         Console.Write(array[i,j]+ " ");
+
+//         if(i==j)
+//         {
+//             sum= sum + array[i,j];
+//         }
+//     }
+//     Console.WriteLine();
+// }
+
+//  Console.WriteLine("Сумма элементов на главной диагонали = "+ sum);
+// Задача 53: Задайте двумерный массив. Напишите программу, 
+// которая поменяет местами первую и последнюю строку массива.
+
+// Console.WriteLine("Введите размероность массива");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите размероность массива");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// int[,]  GetArray (int m, int n)
+// {
+//     int [,] array = new int[m,n]; 
+
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i,j] = new Random().Next(0,10);
+//         }
+//     }
+
+//     return array;
+// }
+
+// void PrintArray(int[,] inputArray)    
+// {
+//     for (int i = 0; i < inputArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inputArray.GetLength(1); j++)
+//         {
+//             Console.Write(inputArray[i,j]);
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void ChangeArray(int[,] matrix)
+// {
+//     int rowsCount = matrix.GetLength(0)-1;
+
+//     for (int i = 0; i < matrix.GetLength(1); i++)
+//     {
+//         int temp = matrix[0,i];
+//         matrix[0,i] =matrix[rowsCount,i];
+//         matrix[rowsCount,i]= temp;
+//     }
+// }
+
+// int[,] array2= GetArray(rows,columns);
+// PrintArray(array2);
+// ChangeArray(array2);
+// System.Console.WriteLine();
+// PrintArray(array2);
+// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет
+//  строки на столбцы. В случае, если это невозможно, программа должна 
+//  вывести сообщение для пользователя.
+
+// Console.WriteLine("Введите размероность массива");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// int [,] array = new int[rows,rows]; 
+// int [,] array2 = new int[rows,rows]; 
+
+// int[,]  GetArray (int [,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i,j] = new Random().Next(0,10);
+//         }
+//     }
+
+//     return array;
+// }
+
+// void PrintArray(int[,] inputArray)    
+// {
+//     for (int i = 0; i < inputArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inputArray.GetLength(1); j++)
+//         {
+//             Console.Write(inputArray[i,j]);
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void ChangeArray(int[,] matrix,int[,] matrix2 )
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//            matrix2[j,i]= matrix[i,j];
+//         }
+//     }
+// }
+
+// GetArray(array);
+// PrintArray(array);
+// ChangeArray(array,array2);
+// System.Console.WriteLine();
+// PrintArray(array2);
+
+// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет
+//  строки на столбцы. В случае, если это невозможно, программа должна 
+//  вывести сообщение для пользователя.
+
+// Console.WriteLine("Введите размероность массива");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите размероность массива");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// if(rows != columns)
+// {
+//     System.Console.WriteLine("Введите правильные числа");
+//     return;
+// }
+
+// int [,] array = new int[rows,columns]; 
+// int [,] array2 = new int[rows,columns]; 
+
+// int[,]  GetArray (int [,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i,j] = new Random().Next(0,10);
+//         }
+//     }
+
+//     return array;
+// }
+
+// void PrintArray(int[,] inputArray)    
+// {
+//     for (int i = 0; i < inputArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inputArray.GetLength(1); j++)
+//         {
+//             Console.Write(inputArray[i,j]);
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void ChangeArray(int[,] matrix,int[,] matrix2 )
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//            matrix2[j,i]= matrix[i,j];
+//         }
+//     }
+// }
+
+// GetArray(array);
+// PrintArray(array);
+// ChangeArray(array,array2);
+// System.Console.WriteLine();
+// PrintArray(array2);
+// Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, 
+// которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Наименьший элемент - 1, на выходе получим 
+// следующий массив:
+
+
+// Console.WriteLine("Введите размероность массива");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите размероность массива");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// int minValue= int.MaxValue;
+// int minRows=0;
+// int minColumns= 0;
+
+// int[,] GetArray (int m, int n)
+// {
+//     int [,] array = new int[m,n]; 
+
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i,j] = new Random().Next(0,10);
+
+//             if(array[i,j] <minValue)
+//             {
+//                 minValue = array[i,j];
+//                 minRows= i;
+//                 minColumns = j;
+//             }
+//         }
+//     }
+
+//     return array;
+// }
+
+// void PrintArray(int[,] inputArray)    
+// {
+//     for (int i = 0; i < inputArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inputArray.GetLength(1); j++)
+//         {
+//             Console.Write(inputArray[i,j]);
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void ChangeArray(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (minRows!= i && minColumns!=j)
+//             {
+//                 Console.Write(matrix[i,j]);
+//             }
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int[,] array2= GetArray(rows,columns);
+// PrintArray(array2);
+// System.Console.WriteLine();
+// ChangeArray(array2);
+
+// Задача 63: Задайте значение N. Напишите
+//  программу, которая выведет все натуральные 
+//  числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// string PrintNumbers(int start, int end)
+// {
+//     if(start == end)
+//     {
+//         return start.ToString();
+//     }
+//     return start+ " "+ PrintNumbers(start+1,end);
+// }
+
+// System.Console.WriteLine(PrintNumbers(1, num));
